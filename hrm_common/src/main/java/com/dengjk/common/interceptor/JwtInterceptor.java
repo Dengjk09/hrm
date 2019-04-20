b9e10a5cd370232a9a882e5e91e2f4767f4ccdc1
djk <277991520@qq.com>
2019-04-21 00:39:05 +0800

    后端权限拦截生成,权限控制到api

--- hrm_common/src/main/java/com/dengjk/common/interceptor/JwtInterceptor.java
+++ hrm_common/src/main/java/com/dengjk/common/interceptor/JwtInterceptor.java
@@ -11,7 +11,6 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.invoke.MethodHandle;
 
 /**
  * @author Dengjk
